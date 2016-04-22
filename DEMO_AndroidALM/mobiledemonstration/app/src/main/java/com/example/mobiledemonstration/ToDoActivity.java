ac0eb6868d0c403c05aef5fb65215c80e97e0433
ahriman-ru <ahriman@ahriman.ru>
2016-04-22 09:00:29 +0300

    1

--- DEMO_AndroidALM/mobiledemonstration/app/src/main/java/com/example/mobiledemonstration/ToDoActivity.java
+++ DEMO_AndroidALM/mobiledemonstration/app/src/main/java/com/example/mobiledemonstration/ToDoActivity.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.AsyncTask;
